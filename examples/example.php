aebfd1dcee30c2a3b5cd4016f13502ab9a300049
Lars Olesen <lars@intraface.dk>
2007-09-03 19:07:07 +0000

    Updated example to make it clearer.

--- examples/example.php
+++ examples/example.php
@@ -3,10 +3,13 @@ set_include_path('../src/Validator/' . PATH_SEPARATOR . get_include_path());
 
 require_once 'Validator.php';
 
+// either filled like this or automatically from GET and POST
+$request = new Request_Raw(array('username' => 'testervih.dk'));
+
 $validator = new Validator;
 $validator->add(new Validator_Basic(new Rule_Email('username'), 'email is not valid'));
 
-if (!$validator->validate(new Request_Raw(array('username' => 'testervih.dk')))) {
+if (!$validator->validate($request)) {
     print_r($validator->getErrors());
 } else {
     print_r($validator->getCleanRequest());
