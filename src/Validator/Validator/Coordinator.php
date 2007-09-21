7e793a385a4bce149c6aa08afaeef45eb07eebc4
Lars Olesen <lars@intraface.dk>
2007-09-21 11:36:41 +0000

    Added a little documentation, and suggested some kind of memory on the coordinator.

--- src/Validator/Validator/Coordinator.php
+++ src/Validator/Validator/Coordinator.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Essentially this is a stand in for the requests. It helps to make sure, that
- * the validated object is not copied into a clean value object before the values
+ * This is a stand in for the requests. It helps to make sure, that
+ * validated object is not copied into a clean value object before the values
  * are validated.
  *
- * It should be simple to put in a logger object to remember the messages.
+ * Should probably get a logger also, or should we just make it possible using some form of callback?
  */
 class Validator_Coordinator
 {
@@ -12,31 +12,50 @@ class Validator_Coordinator
     private $clean;
     private $errors = array();
 
-    public function __construct($raw,$clean) {
+    public function __construct($raw, $clean)
+    {
         $this->raw = $raw;
         $this->clean = $clean;
     }
 
-    public function get($name) {
+    public function get($name)
+    {
         return $this->raw->getForValidation($name);
     }
 
-    public function setClean($name=FALSE) {
-        if (!$name) return FALSE;
-        $this->clean = $this->clean->set(
-        $name,
-        $this->raw->getForValidation($name));
+    public function setClean($name = false)
+    {
+        if (!$name) {
+                return false;
+        }
+        $this->clean = $this->clean->set($name, $this->raw->getForValidation($name));
     }
 
-    public function addError($error) {
-        $this->errors[] = $error;
+    public function getCleanRequest()
+    {
+        return $this->clean;
     }
 
-    public function getErrors() {
+    // we need to be able to remember the errors for next page
+    public function addError($field, $error)
+    {
+        $this->errors[$field] = $error;
+    }
+
+    public function getErrors()
+    {
         return $this->errors;
     }
 
-    public function getCleanRequest() {
-        return $this->clean;
+    /*
+    public function __sleep() {
+        $properties = array();
+        $class = new ReflectionClass($this);
+        foreach ($class->getProperties() as $property) {
+            $properties[] = $property->getName();
+        }
+        return $properties;
     }
+    */
+
 }
